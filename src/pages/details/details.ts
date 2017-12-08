48040107d70610d4e3546baaa4baf5a2ead835ff
robsberrywan <yrar93@yahoo.com>
2017-12-09 03:45:33 +0800

    a

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,24 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+
+declare var google;
 
 @Component({
   selector: 'page-details',
   templateUrl: 'details.html'
 })
 export class DetailsPage {
+  geocoder;
+  markers;
+  address;
+  polylines = [];
+
   trip;
   legWalk;
   legTransit;
   index;
   description;
-  iosicon;
-  mdicon;
+  modeIcons;
   leg;
+
+  map: any;
+  @ViewChild('map') mapElement: ElementRef;
     constructor(public navCtrl: NavController, public navParams: NavParams){
       this.index = this.navParams.get("i");
       this.trip = this.navParams.get("trip");
       this.legWalk = this.navParams.get("legW");
       this.legTransit = this.navParams.get("legT");
+      this.address = this.navParams.get('address');
+      this.markers = [];
+      this.geocoder = new google.maps.Geocoder;
       this.description = [{
         distance: '',
         time: '',
@@ -28,13 +40,109 @@ export class DetailsPage {
         from: '',
         to: ''
       }];
-      this.iosicon = [];
-      this.mdicon = [];
+      this.modeIcons = [];
       this.leg = [];
     }
     ionViewDidLoad(){
-      console.log(this.trip[this.index].legs);
+      this.loadMap();
       this.buildlist();
+    }
+    loadMap(){
+      let mapOptions = {
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.setOrgDes();
+      this.plotted(this.index);
+    }
+    setOrgDes(){
+      this.geocoder.geocode({'address': this.address.origin}, (results, status) => {
+        if(status == 'OK' && results[0]){
+          let marker = new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: this.map,
+          });
+          this.markers.push(marker);
+        }
+      })
+      this.geocoder.geocode({'address': this.address.destination}, (results, status) => {
+        if(status == 'OK' && results[0]){
+          let marker = new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: this.map,
+          });
+          this.markers.push(marker);
+          this.setMapFocus(this.markers);
+        }
+      })
+    }
+    setMapFocus(markers){
+      let bounds = new google.maps.LatLngBounds();
+      for(let i=0; i<markers.length; i++){
+        bounds.extend(markers[i].getPosition());
+      }
+      this.map.fitBounds(bounds);
+      this.map.setZoom(15);
+    }
+
+    plotted(index){
+      for(let i=0; i<this.trip[index].legs; i++){
+        this.points = [];
+        for(let j=0; j<this.legWalk.length; j++){
+          console.log(this.legWalk[j].seq);
+          if((this.legWalk[j].tripID==this.trip[index].id)&&(this.legWalk[j].seq==i)){
+            console.log("walk");
+            this.decode(this.legWalk[j].legGeom)
+            this.drawSnappedPolyline('blue');
+          }
+        }
+        for(let k=0; k<this.legTransit.length; k++){
+          if((this.legTransit[k].tripID==this.trip[index].id)&&(this.legTransit[k].seq==i)){
+            console.log("transit");
+            this.decode(this.legTransit[k].legGeom);
+            this.drawSnappedPolyline('red');
+          }
+        }
+      }
+    }
+    points;
+    decode(leggeom){
+      // array that holds the points
+      let index = 0, len = leggeom.length;
+      let lat = 0, lng = 0;
+      while (index < len) {
+        let b, shift = 0, result = 0;
+        do {    
+          b = leggeom.charAt(index++).charCodeAt(0) - 63;//finds ascii                                                                                    //and substract it by 63
+          result |= (b & 0x1f) << shift;
+          shift += 5;
+        } while (b >= 0x20);
+      
+        let dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+        lat += dlat;
+        shift = 0;
+        result = 0;
+        do {
+          b = leggeom.charAt(index++).charCodeAt(0) - 63;
+          result |= (b & 0x1f) << shift;
+          shift += 5;
+        } while (b >= 0x20);
+        let dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+        lng += dlng;
+        let latlng = new google.maps.LatLng(( lat / 1E5),( lng / 1E5));
+        this.points.push(latlng);
+      }
+    }
+    drawSnappedPolyline(color){
+      let snappedPolyline = new google.maps.Polyline({
+        path: this.points,
+        strokeColor: color,
+        strokeWeight: 3
+      });
+      
+      snappedPolyline.setMap(this.map);
+      this.polylines.push(snappedPolyline);
     }
     buildlist(){
       this.leg.length = 0;
@@ -51,8 +159,7 @@ export class DetailsPage {
               from: '',
               to: ''
             });
-            this.iosicon.push("ios-walk");
-            this.mdicon.push("md-walk");
+            this.modeIcons.push("./assets/imgs/walk.png");
           }
         }
         for(let k=0; k<this.legTransit.length; k++){
@@ -77,8 +184,7 @@ export class DetailsPage {
                 from: this.legTransit[k].from,
                 to: this.legTransit[k].to
               });
-              this.iosicon.push("ios-car");
-              this.mdicon.push("md-car");
+              this.modeIcons.push("./assets/imgs/jeep.png");
             }
             else if(this.legTransit[k].transMode=="PUB"){
               
@@ -99,12 +205,10 @@ export class DetailsPage {
                 from: this.legTransit[k].from,
                 to: this.legTransit[k].to
               });
-              this.iosicon.push("ios-bus");
-              this.mdicon.push("md-bus");
+              this.modeIcons.push("./assets/imgs/bus.png");
             }
             else{
-              this.iosicon.push("ios-train");
-              this.mdicon.push("md-train");
+              this.modeIcons.push("./assets/imgs/train.png");
             }
           }
         }
@@ -114,5 +218,4 @@ export class DetailsPage {
 
 /*
 -arrange details output
--transfer map from routes to details
 */
--- src/pages/routes/routes.ts
+++ src/pages/routes/routes.ts
@@ -18,10 +18,6 @@ export class RoutesPage {
   geocoder;
   markers;
 
-  snappedCoordinates = [];
-  placeIdArray = [];
-  polylines = [];
-
   trip: any = [{
     id: '',
     transfer : '',
@@ -70,14 +66,12 @@ export class RoutesPage {
       this.geocoder = new google.maps.Geocoder;
       this.markers = [];
       this.trip.length = 0;
-      this.points = [];
     }
 
   ionViewDidLoad(){
     this.loadMap();
     this.setOrgDes();
   }
-
   loadMap(){
     let mapOptions = {
         zoom: 15,
@@ -85,17 +79,11 @@ export class RoutesPage {
     }
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
   }
-  clearMarkers(){
-    for(let i=0; i<this.markers.length; i++)
-      this.markers[i].setMap(null);
-    this.markers = [];
-  }
   /*
     Geocode address string into latitude and longitude for markers.
     Put markers in origin destination then set map focus between the paths.
   */
   setOrgDes(){
-    this.clearMarkers();
     this.geocoder.geocode({'address': this.address.origin}, (results, status) => {
       if(status == 'OK' && results[0]){
         let marker = new google.maps.Marker({
@@ -117,12 +105,6 @@ export class RoutesPage {
     })
   }
   setMapFocus(markers){
-    let bounds = new google.maps.LatLngBounds();
-    for(let i=0; i<markers.length; i++){
-      bounds.extend(markers[i].getPosition());
-    }
-    this.map.fitBounds(bounds);
-    this.map.setZoom(15);
     this.rsp.load(markers[0].getPosition(), markers[1].getPosition())
     .then(data => {
       if(data)
@@ -200,105 +182,15 @@ export class RoutesPage {
       });
     }
   }
-
-  plotted(index){
-    console.log(index);
-    this.drawSnappedPolyline('blue', 0);
-    for(let i=0; i<this.trip[index].legs; i++){
-      this.points = [];
-      for(let j=0; j<this.legWalk.length; j++){
-        console.log(this.legWalk[j].seq);
-        if((this.legWalk[j].tripID==this.trip[index].id)&&(this.legWalk[j].seq==i)){
-          console.log("walk");
-          this.decode(this.legWalk[j].legGeom)
-          this.drawSnappedPolyline('blue', 1);
-        }
-      }
-      for(let k=0; k<this.legTransit.length; k++){
-        if((this.legTransit[k].tripID==this.trip[index].id)&&(this.legTransit[k].seq==i)){
-          console.log("transit");
-          this.decode(this.legTransit[k].legGeom);
-          this.drawSnappedPolyline('red', 1);
-        }
-      }
-    }
-  }
-  points;
-  decode(leggeom){
-    // array that holds the points
-    let index = 0, len = leggeom.length;
-    let lat = 0, lng = 0;
-    while (index < len) {
-      let b, shift = 0, result = 0;
-      do {    
-        b = leggeom.charAt(index++).charCodeAt(0) - 63;//finds ascii                                                                                    //and substract it by 63
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-    
-      let dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lat += dlat;
-      shift = 0;
-      result = 0;
-      do {
-        b = leggeom.charAt(index++).charCodeAt(0) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-      let dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lng += dlng;
-      let latlng = new google.maps.LatLng(( lat / 1E5),( lng / 1E5));
-      this.points.push(latlng);
-    }
-    /*this.rsp.runSnapToRoad(points)
-    .then(snap => {
-      if(snap){
-        console.log(snap);
-        this.processSnapToRoadResponse(snap);
-        this.drawSnappedPolyline();
-      }
-      else
-        console.log("No trip found.");
-    })*/
-  }
-  processSnapToRoadResponse(data) {
-    this.snappedCoordinates = [];
-    this.placeIdArray = [];
-    for (let i = 0; i < data.snappedPoints.length; i++) {
-      let latlng = new google.maps.LatLng(
-          data.snappedPoints[i].location.latitude,
-          data.snappedPoints[i].location.longitude);
-      this.snappedCoordinates.push(latlng);
-      this.placeIdArray.push(data.snappedPoints[i].placeId);
-    }
-  }
-  drawSnappedPolyline(color, num){
-    let snappedPolyline = new google.maps.Polyline({
-      path: this.points,
-      strokeColor: color,
-      strokeWeight: 3
-    });
-    
-    snappedPolyline.setMap(this.map);
-    this.polylines.push(snappedPolyline);
-    if(num==0){
-      snappedPolyline.setMap(null);
-      this.polylines = [];
-    }
-  }
   seeDetails(i){
     let trip: any;
     let legW: any;
     let legT: any;
     trip = this.trip, legW = this.legWalk, legT  = this.legTransit;
-    this.navCtrl.push(DetailsPage, {"i": i, "trip": trip, "legW": legW, "legT": legT}).catch( err => {
+    this.navCtrl.push(DetailsPage, {"i": i, "trip": trip, "legW": legW, "legT": legT, "address": this.address}).catch( err => {
       console.log(err)});
   }
   closeModal(){
     this.viewCtrl.dismiss();
   }
 }
-/*
--processinput error occur if trip is too long
--check array contents (trip, legwalk, legtransit)
-*/
