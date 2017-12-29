28320031e000461d31fbe98b8401905b4163ee81
robsberrywan <yrar93@yahoo.com>
2017-12-30 01:39:14 +0800

    f

--- src/pages/mytrip/mytrip.ts
+++ src/pages/mytrip/mytrip.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
 
 @Component({
     selector: 'page-mytrip',
